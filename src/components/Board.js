561cdcb7c2b92012170197a67cc4b45d6bbf0f59
Andrew Huang <andrew.huang12@gmail.com>
2016-04-14 21:07:13 -0700

    Re-enable all functionality
     * checkBoard() and gameOver overlay working again after tiles-matrix refactor
     * Added .gitignore
     * Added underscore as a dependency
    
     Please npm install

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,13 +4,14 @@ import styles from '../styles/Board.scss';
 export default class Board extends React.Component {
   render() {
     let tiles = this.props.tiles;
-    // let board = [];
 
     let board = tiles.reduce((newBoard, row, rowIndex) => {
-      let rowTiles = row.map((tile, index) => {
+      let rowTiles = row.map((tile, tileIndex) => {
+        // Refer to individual tiles by index (0-8)
+        let position = rowIndex * 3 + tileIndex;
         return <div className='tile'
-                  key={rowIndex * 3 + index}
-                  onClick={this.props.tileClick.bind(this, index)} >
+                  key={position}
+                  onClick={this.props.tileClick.bind(this, tileIndex, rowIndex)} >
                 { tile }
               </div>
       })
@@ -25,34 +26,3 @@ export default class Board extends React.Component {
     );
   }
 }
-
-/*
-map((tile, index) => {
-      return <div className='tile'
-                  key={index}
-                  onClick={this.props.tileClick.bind(this, index)} >
-                { tile }
-              </div>
-    });
-
-    ES6
-    `[...newBoard, ...rowTiles]`` is the same as `newBoard.concat(rowTiles)``
-
-
-        tiles.forEach((row, rowIndex) => {
-      row.forEach((tile, index) => {
-        board.push(<div className='tile'
-                  key={rowIndex * 3 + index}
-                  onClick={this.props.tileClick.bind(this, index)} >
-                { tile }
-              </div>);
-      })
-    });
-
-
-0 1 2
-3 4 5
-6 7 8
-
-
-*/
